68b9725a78dbe0b4fec2e0fb8081468a17e3f5ae
Dor Katzir <katzirdor@gmail.com>
2022-05-04 22:53:15 +0300

    added touch start event

--- canvasBox1.js
+++ canvasBox1.js
@@ -38,6 +38,13 @@ canvas.addEventListener('mousemove', (e) => {
     //touchcancel -
     //touchleave
     //touchmove - occurs when the user moves the finger across the screen
+canvas.addEventListener('touchstart', function(event) {
+    mouse.x = e.x
+    mouse.y = e.y
+    for (let i = 0; i < 2; i++) {
+        particlesArray.push(new Particle())
+    }
+});
 canvas.addEventListener('touchmove', function(event) {
     mouse.x = e.x
     mouse.y = e.y
